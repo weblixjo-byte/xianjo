e4a1255329b3f2a93d48719888e8b204fbb71fb2
Mohammad Alseidi <mfaraj981@gmail.com>
2026-03-21 20:55:36 +0300

    image6

--- next.config.ts
+++ next.config.ts
@@ -4,9 +4,7 @@ const nextConfig: NextConfig = {
   reactCompiler: true,
   images: {
     remotePatterns: [
-      { protocol: "https", hostname: "images.unsplash.com" },
-      { protocol: "https", hostname: "placehold.co" },
-      { protocol: "https", hostname: "lh3.googleusercontent.com" },
+      { protocol: "https", hostname: "**" },
     ],
   },
   async headers() {
