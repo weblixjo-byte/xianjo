a0ba056930e61f15e3511bd9a6ffa0aeb6b3c698
Mohammad Alseidi <mfaraj981@gmail.com>
2026-05-28 23:44:57 +0300

    update 13

--- src/app/admin/layout.tsx
+++ src/app/admin/layout.tsx
@@ -2,6 +2,11 @@ import type { Metadata } from "next";
 
 export const metadata: Metadata = {
   manifest: "/manifest-admin.json",
+  icons: {
+    icon: "/OG-IMG.png",
+    shortcut: "/OG-IMG.png",
+    apple: "/OG-IMG.png",
+  },
 };
 
 export default function AdminLayout({
